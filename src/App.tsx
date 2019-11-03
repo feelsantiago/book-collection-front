a37f6334cca796c9ebaa332a65ca214a2fb02a3e
Filipe Santiago <filipe@upplify.com>
2019-11-03 14:40:14 -0300

    remove graphql components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React, { FunctionComponent } from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
 import RoutesModule from './routes/route-module';
 import routes from './routes';
-import client from './graphql/client';
 
 const App: FunctionComponent = () => {
-	return (
-		<ApolloProvider client={client}>
-			<RoutesModule routes={routes} />
-		</ApolloProvider>
-	);
+	return <RoutesModule routes={routes} />;
 };
 
 export default App;
